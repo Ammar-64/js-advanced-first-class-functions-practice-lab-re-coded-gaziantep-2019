32f7f23ece3aa7c130a162e918ade6dc846fb6d0
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-18 00:59:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function logDriverNames(drivers){
   return drivers.forEach(driver => console.log(driver.name));
 }
+function logDriversByHometown(drivers, hometown) {
+  drivers.forEach(function(driver) {
+    if (driver.hometown === hometown) { console.log(driver.name) }
+  })
+}
