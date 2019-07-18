c3ec5812a22324d56d321d70dfe85967ccbeea9d
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-18 01:07:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+  return drivers.forEach(driver => console.log(driver.name));
+}
+function logDriversByHometown(drivers, hometown) {
+  drivers.forEach(function(driver) {
+    if (driver.hometown === hometown) { console.log(driver.name) }
+  })
+}
+function driversByRevenue(drivers) {
+  return drivers.slice().sort( (a, b) => {return a.revenue - b.revenue} )
+}
+function driversByName(drivers) {
+  return drivers.slice().sort( (a, b) => {return a.name.localeCompare(b.name)} )
+}
+function totalRevenue(drivers) {
+  let total = 0;
+  return drivers.reduce((total, driver) => {return total += driver.revenue}, 0)
+}
+function averageRevenue(drivers) {
+  return totalRevenue(drivers) / drivers.length
+}
