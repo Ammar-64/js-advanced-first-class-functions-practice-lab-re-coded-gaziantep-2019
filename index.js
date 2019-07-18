7e3845ebc4537fc2c82aa15feabbf09a71d4e392
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-18 00:55:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file!
-function logDriverNames(el){
-  return drivers.forEach(console.log(el));
+const driversNames = function logDriverNames(el){
+  return console.log(drivers.name);
 }
+drivers.forEach(driversNames)
